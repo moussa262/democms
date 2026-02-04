638687bd836da5698ca97f4ef4d49fd3fe484566
moussa262 <laialm2026@gmail.com>
2026-02-04 23:17:31 +0100

    added all files

---
+++ src/main/java/com/cms_project/democms/DemocmsApplication.java
@@ -0,0 +1,13 @@
+package com.cms_project.democms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cms_project/democms/DemocmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms_project.democms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
